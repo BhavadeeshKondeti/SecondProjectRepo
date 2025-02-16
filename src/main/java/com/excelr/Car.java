cbef6942720da10709a1d890ea407c5c45cd22b7
KondetiBhavadeesh <bhavadeeshkondeti34@gmail.com>
2025-02-16 17:33:25 +0530

    update in Car

--- src/main/java/com/excelr/Car.java
+++ src/main/java/com/excelr/Car.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class Car {
+	
+	int a=55;
 
 	public Car() {
 		System.out.println("car constructor executed");
 	}
+	
+
 	
 	
 
