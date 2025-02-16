5411615b21fa832c3f2270cbd7f4f502c29ede12
KondetiBhavadeesh <bhavadeeshkondeti34@gmail.com>
2025-02-16 16:51:51 +0530

    Second pro all files added

---
+++ src/main/java/com/excelr/Car.java
@@ -0,0 +1,15 @@
+package com.excelr;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class Car {
+
+	public Car() {
+		System.out.println("car constructor executed");
+	}
+	
+	
+
+}
