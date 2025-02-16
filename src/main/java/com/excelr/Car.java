bf3bddf34d9d13f7dc0594c34f2c649e79ca4107
KondetiBhavadeesh <bhavadeeshkondeti34@gmail.com>
2025-02-16 18:12:47 +0530

    Revert "update in Car"
    This reverts commit cbef6942720da10709a1d890ea407c5c45cd22b7.

--- src/main/java/com/excelr/Car.java
+++ src/main/java/com/excelr/Car.java
@@ -5,14 +5,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class Car {
-	
-	int a=55;
 
 	public Car() {
 		System.out.println("car constructor executed");
 	}
-	
-
 	
 	
 
